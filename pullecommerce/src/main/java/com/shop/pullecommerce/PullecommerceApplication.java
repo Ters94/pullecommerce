c35126d945044eb2cc7fc138824a9c3edbc0ac46
Marwa Ters <marwaters94@gmail.com>
2025-05-28 10:41:56 +0200

    Initial commit

---
+++ pullecommerce/src/main/java/com/shop/pullecommerce/PullecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.shop.pullecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PullecommerceApplication.class, args);
+	}
+
+}
---
+++ pullecommerce/src/test/java/com/shop/pullecommerce/PullecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.pullecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
