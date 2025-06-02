7731ab4a07de93c1dda1acb8e318273f4dbdb642
Marwa Ters <marwaters94@gmail.com>
2025-06-02 10:46:47 +0200

    creation data pour tester

---
+++ pullecommerce/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category(id, name) VALUES
+(1, 'Pulls Hiver'),
+(2, 'Pulls Été');
+
+INSERT INTO product(id, name, description, price, category_id) VALUES
+(1, 'Pull bleu', 'Chaud et confortable', 45.0, 1),
+(2, 'Pull rouge', 'Design moderne', 39.0, 1),
+(3, 'Pull léger', 'Idéal pour l’été', 29.0, 2);
