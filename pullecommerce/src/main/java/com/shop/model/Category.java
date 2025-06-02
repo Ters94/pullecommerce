7706a9c302c4c33efca50283bc6b637c3b827347
Marwa Ters <marwaters94@gmail.com>
2025-06-02 10:17:08 +0200

    annotation Data pour les getters /setters/controller

--- pullecommerce/src/main/java/com/shop/model/Category.java
+++ pullecommerce/src/main/java/com/shop/model/Category.java
@@ -3,7 +3,9 @@ package com.shop.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
 @Entity
 public class Category {
     @Id @GeneratedValue
--- pullecommerce/src/main/java/com/shop/model/Product.java
+++ pullecommerce/src/main/java/com/shop/model/Product.java
@@ -3,7 +3,8 @@ package com.shop.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-
+import lombok.Data;
+@Data
 @Entity
 public class Product {
     @Id @GeneratedValue
