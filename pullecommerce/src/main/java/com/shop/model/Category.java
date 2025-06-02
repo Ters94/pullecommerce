bb3bef44b63c5a282b0a882388df2506418bb6b6
Marwa Ters <marwaters94@gmail.com>
2025-06-02 09:18:04 +0200

    creation des entités product&categ

---
+++ pullecommerce/src/main/java/com/shop/model/Category.java
@@ -0,0 +1,14 @@
+package com.shop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    
+
+}
---
+++ pullecommerce/src/main/java/com/shop/model/Product.java
@@ -0,0 +1,18 @@
+package com.shop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id @GeneratedValue
+   
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    
+    
+
+}
